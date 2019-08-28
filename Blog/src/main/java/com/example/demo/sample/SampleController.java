7d6aa87af2615a253f7916a94f3ac9e2eede7ea2
Takakura Kaede <ex4ngat0cf@au.com>
2019-08-29 02:43:23 +0900

    add: form

--- Blog/src/main/java/com/example/demo/sample/SampleController.java
+++ Blog/src/main/java/com/example/demo/sample/SampleController.java
@@ -1,7 +1,10 @@
 package com.example.demo.sample;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SampleController {
@@ -11,4 +14,19 @@ public class SampleController {
 		
 		return "sample";	//sample.htmlを表示
 	}
+	
+	@PostMapping("/sample")	//Postメソッドの処理
+	public String postRequest(@RequestParam("text")String str,Model model) {	//sample.htmlのnameからの値を受け取る
+		
+		model.addAttribute("response",str);	//htmlから値を受け取れる。model.addAttribute("キー名", 値)
+		
+		return "sampleResponse";
+	}
 }
+
+/* GET POST 違い
+ * ブラウザからサーバー　リクエスト
+ * サーバーからブラウザ　レスポンス
+ * GETは目でパラメータが見える、ブックマーク出来る、取得する
+ * POSTは目で見えない、ブックマーク出来ない、新しく登録する、POSTのみでも作れる(よくない)*/
+
