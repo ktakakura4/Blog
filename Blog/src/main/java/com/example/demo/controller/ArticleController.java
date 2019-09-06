499b61f318cf721f1b1a1c44c5158478dce3df3c
Takakura Kaede <ex4ngat0cf@au.com>
2019-09-07 00:41:00 +0900

    add: form

--- Blog/src/main/java/com/example/demo/controller/ArticleController.java
+++ Blog/src/main/java/com/example/demo/controller/ArticleController.java
@@ -4,17 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ArticleController {
 	
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num, Model model) {
-		int res = 0;
-		for (int i = 1;i <= num;i++)
-			res += i;
-		model.addAttribute("msg", "total:" + res);
-		return "index";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "お名前を書いて送信してください");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1")String str, ModelAndView mav) {
+		mav.addObject("msg", "こんにちは" + str + "さん");
+		mav.addObject("value", str);
+		mav.setViewName("index");
+		return mav;
 	}
 
 }
