17f8e44d1e5a2fbd0f4dcacdba2ad9495d805e95
Takakura Kaede <ex4ngat0cf@au.com>
2019-09-07 00:20:56 +0900

    add: msg

--- Blog/src/main/java/com/example/demo/controller/ArticleController.java
+++ Blog/src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ArticleController {
 	
-	@RequestMapping("/")
-	public String index() {
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model) {
+		int res = 0;
+		for (int i = 1;i <= num;i++)
+			res += i;
+		model.addAttribute("msg", "total:" + res);
 		return "index";
 	}
 
