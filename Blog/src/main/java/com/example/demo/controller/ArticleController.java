7f570dda4381a631ebd5b9597671f12a7ddeb62d
Takakura Kaede <ex4ngat0cf@au.com>
2019-09-10 02:39:56 +0900

    add: Delete

--- Blog/src/main/java/com/example/demo/controller/ArticleController.java
+++ Blog/src/main/java/com/example/demo/controller/ArticleController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,4 +47,22 @@ public class ArticleController {
 		model.addAttribute("contents", "article/list :: list_contents");
 		return "redirect:/articlelist";
 	}
+
+	@GetMapping("article/{id}/detail")
+	public String getDetail(Model model) {
+		return "article/detail";
+	}
+
+	@GetMapping("article/{id}/update")
+	public String getUpdate(Model model) {
+		return "article/update";
+	}
+
+	@PostMapping("article/{id}")
+	public String delete(@PathVariable Long id){
+		articleService.delete(id);
+		return "redirect:/articlelist";
+	}
+	
+	
 }
